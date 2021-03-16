08638293a33bfe90c31043fa915529ced95709be
twigz826 <thomastwigden@gmail.com>
2021-03-16 13:03:21 -0300

    Working on the text box for user input

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Text>Enter mileage:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. 50'
+        // onChangeText = () =>
+        />
       <Button color="orange" title="Click Me" onPress={() => console.log("Button tapped")}/>
     </SafeAreaView>
   );
@@ -17,4 +23,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 100,
+  }
 });
