885ea890be024ec5a09189f169ba14a97ec29bb8
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-16 16:16:43 +0000

    Added mileage value

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [miles, setMiles] = useState('0')
   return (
     <SafeAreaView style={styles.container}>
       <Text>Enter mileage:</Text>
       <TextInput
         style={styles.input}
         placeholder='e.g. 50'
-        // onChangeText = () =>
+        onChangeText={(val) => setMiles(val)} 
         />
-      <Button color="orange" title="Click Me" onPress={() => console.log("Button tapped")}/>
+      <Button color="orange" title="Click Me" onPress={() => console.log(miles)}/>
     </SafeAreaView>
   );
 }
