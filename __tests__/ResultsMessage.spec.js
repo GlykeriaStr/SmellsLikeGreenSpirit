c3c8e2f57c22d2659f898d4f510ada5dce22dd99
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-22 11:49:30 +0000

    Dishwasher to showering context given

--- __tests__/ResultsMessage.spec.js
+++ __tests__/ResultsMessage.spec.js
@@ -1,12 +1,15 @@
 import resultsMessage  from '../src/logic/ResultsMessage';
 
 describe('ResultsMessage', () => {
-  const compliment = "Well done! Thanks for thinking about the planet"
+  const compliment = "Well done! Thanks for thinking about the planet."
   const kettleTube = context("boiling a kettle or watching 10hrs of YouTube")
   const gasHob = context("an average meal being cooked on the hob")
   const washingMachine = context("putting one load of laundry in the washing machine")
-  const microwave = context("using the micro wave")
-  
+  const microwave = context("using the microwave")
+  const dishwasher = context("using the dishwasher")
+  const weekendTV = context("a weekend watching TV")
+  const showering = context("showering every day for a week")
+
   it('Compliments user if they have 0 emissions', () => {
     expect(resultsMessage(0)).toEqual(compliment);
   });
@@ -26,8 +29,20 @@ describe('ResultsMessage', () => {
   it('Compares users emissions to microwave', () => {
     expect(resultsMessage(0.4)).toEqual(microwave);
   });
+
+  it('Compares users emissions to dishwasher', () => {
+    expect(resultsMessage(0.6)).toEqual(dishwasher)
+  });
+
+  it('Compares users emissions to a weekend of watching TV', () => {
+    expect(resultsMessage(1.5)).toEqual(weekendTV)
+  })
+
+  it('Compares users emissions to showering every day for a week', () => {
+    expect(resultsMessage(3.5)).toEqual(showering)
+  })
 });
 
 function context(comparison) {
-  return `This is equivalent to ${comparison}`
+  return `This is equivalent to ${comparison}.`
 }
--- src/logic/ResultsMessage.js
+++ src/logic/ResultsMessage.js
@@ -1,7 +1,7 @@
 function resultsMessage(result) {
   switch (true) {
     case(result === 0):
-      return "Well done! Thanks for thinking about the planet";
+      return "Well done! Thanks for thinking about the planet.";
     case(result < 0.06):
       return context("boiling a kettle or watching 10hrs of YouTube")
     case(result < 0.2):
@@ -9,13 +9,20 @@ function resultsMessage(result) {
     case(result < 0.3):
       return context("putting one load of laundry in the washing machine")
     case(result < 0.5):
-      return context("using the micro wave")
+      return context("using the microwave")
+    case(result < 0.7):
+      return context("using the dishwasher")
+    case(result < 2):
+      return context("a weekend watching TV")
+    case(result < 5):
+      return context("showering every day for a week")
+
   }
 }
 
 export default resultsMessage 
 
 function context(comparison) {
-  return `This is equivalent to ${comparison}`
+  return `This is equivalent to ${comparison}.`
 }
 
