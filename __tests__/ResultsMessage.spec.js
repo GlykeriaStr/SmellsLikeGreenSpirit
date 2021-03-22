69506333123e614a9056bb8cd6f9cdbb99ec3b28
Karsten Finlay <karstenfinlay@hotmail.com>
2021-03-22 11:36:19 +0000

    Context given for gas hob to mircowave

--- __tests__/ResultsMessage.spec.js
+++ __tests__/ResultsMessage.spec.js
@@ -3,6 +3,9 @@ import resultsMessage  from '../src/logic/ResultsMessage';
 describe('ResultsMessage', () => {
   const compliment = "Well done! Thanks for thinking about the planet"
   const kettleTube = context("boiling a kettle or watching 10hrs of YouTube")
+  const gasHob = context("an average meal being cooked on the hob")
+  const washingMachine = context("putting one load of laundry in the washing machine")
+  const microwave = context("using the micro wave")
   
   it('Compliments user if they have 0 emissions', () => {
     expect(resultsMessage(0)).toEqual(compliment);
@@ -10,6 +13,18 @@ describe('ResultsMessage', () => {
 
   it('Compares users emissions to boiling the kettle', () => {
     expect(resultsMessage(0.05)).toEqual(kettleTube);
+  });
+
+  it('Compares users emissions to gas hob', () => {
+    expect(resultsMessage(0.15)).toEqual(gasHob);
+  });
+
+  it('Compares users emissions to washing machine', () => {
+    expect(resultsMessage(0.25)).toEqual(washingMachine);
+  });
+
+  it('Compares users emissions to microwave', () => {
+    expect(resultsMessage(0.4)).toEqual(microwave);
   });
 });
 
--- src/logic/ResultsMessage.js
+++ src/logic/ResultsMessage.js
@@ -4,11 +4,18 @@ function resultsMessage(result) {
       return "Well done! Thanks for thinking about the planet";
     case(result < 0.06):
       return context("boiling a kettle or watching 10hrs of YouTube")
+    case(result < 0.2):
+      return context("an average meal being cooked on the hob")
+    case(result < 0.3):
+      return context("putting one load of laundry in the washing machine")
+    case(result < 0.5):
+      return context("using the micro wave")
   }
 }
 
 export default resultsMessage 
-export function context(comparison) {
+
+function context(comparison) {
   return `This is equivalent to ${comparison}`
 }
 
