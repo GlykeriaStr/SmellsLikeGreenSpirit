8f1202b4fd6640b33794bf4e600a6cf1f5476bf0
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-22 15:53:54 +0000

    Added link to the Results page that directs you to the off app payment

--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import emissionsCalculator from '../logic/EmissionsCalculation';
 import { resultsMessage, context } from '../logic/ResultsMessage';
+import * as Linking from 'expo-linking';
+
 
 const ResultsScreen = ({ route }) => {
   const { convertedDistance, emissions } = route.params;
@@ -13,6 +15,19 @@ const ResultsScreen = ({ route }) => {
       <Text>Your journey will release {result} kilograms of CO2.</Text>
       <Text>{'\n'}</Text>
       <Text>{comparison}</Text>
+      <Text>{'\n'}</Text>
+      {result !== 0 ? 
+        <Text
+        style={styles.link}
+        onPress={() =>
+          Linking.openURL(
+            'https://www.clevel.co.uk/offset-a-desired-amount-of-co2/',
+          )
+        }>
+        How you can help!
+      </Text>:null
+      }
+      
     </View>
   );
 };
@@ -30,6 +45,9 @@ const styles = StyleSheet.create({
     padding: 8,
     margin: 10,
     width: 100,
+  },
+  link: {
+    color: 'blue',
   },
 });
 
