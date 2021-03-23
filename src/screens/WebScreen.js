b81cc169768ed8c62ab3193e224d80df5a0d7961
Miranda Wilson <mirandascwilson@gmail.com>
2021-03-23 16:13:37 +0000

    Seeing a injected JS window pop up

--- src/screens/WebScreen.js
+++ src/screens/WebScreen.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
+import { View } from 'react-native';
 import * as Progress from 'react-native-progress';
 import { WebView } from 'react-native-webview';
-import { View } from 'react-native';
 
 export default MyWeb = ({ route }) => {
   const [progress, setProgress] = useState(0);
   const [isLoaded, setLoaded] = useState(false);
   const { result } = route.params;
-  const code = `console.log('This is javascript')`;
+  const code = "console.log('This is javascript')";
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       {!isLoaded ? (
         <Progress.Bar
           progress={progress}
@@ -21,16 +21,19 @@ export default MyWeb = ({ route }) => {
       ) : null}
       <WebView
         source={{
-          uri: 'https://www.clevel.co.uk/offset-a-desired-amount-of-co2#gform_wrapper_7',
+          uri:
+            'https://www.clevel.co.uk/offset-a-desired-amount-of-co2#gform_wrapper_7',
         }}
         style={{ marginTop: 20 }}
-        onMessage={(event) => {
-          alert(event.nativeEvent.title);
+        onMessage={() => console.log('onMessage')}
+        // javaScriptEnabled={true}
+        injectedJavaScript="window.alert('hello')"
+        onLoadProgress={({ nativeEvent }) => {
+          setProgress(nativeEvent.progress), console.log('onloadprogress');
         }}
-        javaScriptEnabled={true}
-        injectedJavaScript={`console.log('hello')`}
-        onLoadProgress={({ nativeEvent }) => setProgress(nativeEvent.progress)}
-        onLoadEnd={() => setLoaded(true)}
+        onLoadEnd={() => {
+          setLoaded(true), console.log('onloadend');
+        }}
       />
     </View>
   );
