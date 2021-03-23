a52b11902de3a0c48f23a1d24a9d556fc6586513
bullhornfixie <dawson.jon@hotmail.co.com>
2021-03-23 11:59:43 +0000

    installed secure storage and ran failing test

---
+++ src/storage/SecureStorage.js
@@ -0,0 +1,19 @@
+import * as SecureStore from 'expo-secure-store'
+
+export async function save(key, value) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+export async function getValueFor(key) {
+  let result = await SecureStore.getItemAsync(key);
+  console.log(result)
+  if (result) {
+    alert("🔐 Here's your value 🔐 \n" + result);
+  } else {
+    alert('No values stored under that key.');
+  }
+}
+
+
+
+
