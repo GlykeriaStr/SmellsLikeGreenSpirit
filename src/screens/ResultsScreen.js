7c2db30c979ad4a9ff20a27e2dd72dd1aed97f31
Miranda Wilson <mirandascwilson@gmail.com>
2021-03-23 11:23:15 +0000

    Add Webview to results page

--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -1,9 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Webview } from 'react-native-webview';
 import emissionsCalculator from '../logic/EmissionsCalculation';
-import { resultsMessage, context } from '../logic/ResultsMessage';
-import * as Linking from 'expo-linking';
-
+import { resultsMessage } from '../logic/ResultsMessage';
 
 const ResultsScreen = ({ route }) => {
   const { convertedDistance, emissions } = route.params;
@@ -16,18 +15,19 @@ const ResultsScreen = ({ route }) => {
       <Text>{'\n'}</Text>
       <Text>{comparison}</Text>
       <Text>{'\n'}</Text>
-      {result !== 0 ? 
+      {result !== 0 ? (
         <Text
-        style={styles.link}
-        onPress={() =>
-          Linking.openURL(
-            'https://www.clevel.co.uk/offset-a-desired-amount-of-co2/',
-          )
-        }>
-        How you can help!
-      </Text>:null
-      }
-      
+          style={styles.link}
+          onPress={() => (
+            <Webview
+              source={{
+                uri: 'https://www.clevel.co.uk/offset-a-desired-amount-of-co2/',
+              }}
+            />
+          )}>
+          How you can help!
+        </Text>
+      ) : null}
     </View>
   );
 };
